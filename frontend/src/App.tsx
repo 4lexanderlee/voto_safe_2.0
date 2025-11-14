d3b0aea794e09b155df9dfc8ac90b109d7cca4d3
Alexander Lee <melgarejorom@gmail.com>
2025-11-14 12:31:08 -0500

    initial: Carpeta inicial, no estructurada

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import { BrowserRouter, createBrowserRouter, Route, RouterProvider, Routes } from 'react-router-dom'
+import './App.css'
+import LandingPage from './pages/LandingPage';
+
+function App() {
+  
+
+  return (
+    <>
+       <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LandingPage/>}/>
+        </Routes>
+       </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/LandingPage.tsx
@@ -0,0 +1,16 @@
+
+
+const LandingPage = ()=> {
+
+    return (
+        <>
+        <div className="min-w-full bg-red-500">
+            <p className="text-blue-900">holas</p>
+        </div>
+
+        </>
+    )
+}
+
+
+export default LandingPage;
